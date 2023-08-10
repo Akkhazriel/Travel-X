366ace32f21f6efcb104ee2e92d1a6d2ff3ce272
Akkhazriel <akkhazriel@gmail.com>
2023-08-10 18:28:47 +0300

    Travel X Landing page v. 1.0.0

---
+++ src/js/index.js
@@ -0,0 +1,34 @@
+import Swiper from 'swiper/bundle';
+
+// import styles bundle
+import 'swiper/css/bundle';
+
+// init Swiper:
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    parallax: true,
+    speed: 2000,
+
+    keyboard: {
+        enable: true,
+    },
+  
+    // If we need pagination
+    pagination: {
+      el: '.slider-controls__count',
+      type: 'fraction',
+    },
+  
+    // Navigation arrows
+    navigation: {
+        prevEl: '#sliderPrew',
+        nextEl: '#sliderNext',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
